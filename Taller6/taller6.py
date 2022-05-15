ad14c2aa5a81c5c8e718cac8e00ba62b4b241926
Sebastian Bravo <bravos.js@javeriana.edu.co>
2022-05-14 19:36:38 -0500

    k-means

--- Taller6/taller6.py
+++ Taller6/taller6.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt # gráficos
 from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
+import random
 
 # Importar datos
 dt = np.genfromtxt('basketball.dat', skip_header=8, delimiter=',')
@@ -70,7 +71,6 @@ plt.ylabel('Distancia')
 plt.grid()
 dendrogram(z, leaf_rotation=90)
 
-
 # k means determine k
 distortions = []
 K = range(1,30)
@@ -86,4 +86,56 @@ plt.xlabel('k')
 plt.ylabel('Distorisión')
 plt.title('Método de codo')
 plt.grid()
+
+# K means
+k = 2 # 2 clases
+
+# 1.) Definicion de hiperparametros:
+epochs_kmeans = 500 # epocas del aprendizaje no supervisado
+
+# 2.) Fase 1 (Aprendizaje no supervisado - k-means):
+# centroides
+c = np.zeros((k, np.size(matrix, axis=1))) # Inicializacion de la matriz de centroides
+for i in range(k):
+    for j in range(np.size(c, axis=1)):
+        c[i,j] = random.uniform(min(matrix[:,j]), max(matrix[:,j])) # Inicializacion aleatoria
+
+# Entrenamiento de k-means:
+min_d = np.zeros(len(matrix)) # inicializacion del vector de minima distancia
+
+print('Entrenamiento de k-means:\n')
+
+for i in range(epochs_kmeans):
+    print(f'Epoca: {i}')
+    # Calcula de distancia a cada centroide:
+    for j in range(len(matrix)):
+        eu_d = np.sqrt(np.sum((matrix[j] - c)**2, axis=1)) # calculo de la distancia euclidiana
+        min_d[j] = np.argmin(eu_d) # se escoge el centroide de menor distancia
+
+    # Agrupacion de datos segun los centroides y actualizacion de los centroides
+    for l in range(k):
+        cluster = []
+        for m in range(len(matrix)):
+            if min_d[m] == l:
+                cluster.append(matrix[m]) # agrupacion de datos del mismo cluster
+            if len(cluster) == 1: # Si solo existe un dato, el centroide se desplaza a la posicion de este
+                c[l] = np.asarray(cluster)
+            elif len(cluster) > 1: # Si el cluster tiene varios datos, el centroide se actualiza de acuerdo a la media aritemtica
+                c[l] = np.mean(np.asarray(cluster), axis=0) 
+
+cluster1 = matrix[np.where(min_d==0)]
+cluster2 = matrix[np.where(min_d==1)]
+
+# Visualizacion de datos
+plt.figure(5)
+ax = plt.axes(projection ="3d")
+ax.scatter3D(cluster1[:,1], cluster1[:,3], cluster1[:,4], c='red', label='Clase a')
+ax.scatter3D(cluster2[:,1], cluster2[:,3], cluster2[:,4], c='blue', label='Clase b')
+plt.title('Conjunto de entrenamiento datos data_3D ')
+ax.set_xlabel('Atributo 1')
+ax.set_ylabel('Atributo 2')
+ax.set_zlabel('Atributo 3')
+plt.legend()
+plt.grid()
+
 plt.show()
